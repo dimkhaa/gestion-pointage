11562f2967a26696eb505e31ceb3cd019cfd2104
Khadim <khadim.khouma.pro@gmail.com.com>
2019-02-02 21:13:32 +0000

    Création model et migration

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
-<!-- Footer -->
-<footer class="footer">
+     <!-- Footer -->
+      <footer class="footer">
         <div class="row align-items-center justify-content-xl-between">
           <div class="col-xl-6">
             <div class="copyright text-center text-xl-left text-muted">
@@ -23,4 +23,4 @@
             </ul>
           </div>
         </div>
-      </footer>
+      </footer>  
